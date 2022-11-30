014c1c3663210d5efaeb0f5e771d9782e4bee231
NickSolante <41457938+NickSolante@users.noreply.github.com>
2022-11-30 21:42:36 +1100

    feat: terser plugin for webpack

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,8 +1,13 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = () => ({
   output: {
     filename: 'bundle.js',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   module: {
     rules: [
